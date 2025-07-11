961f83885ccf63e4f2c68909af58dc338e88903e
trihound21 <toddmoran@hotmail.com>
2025-07-11 12:44:05 -0400

    Adding practice tasks.

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -18,3 +18,12 @@ function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
 
+// Practice task
+// In this task you need to create a reset button
+// which will reset the followers count code.
+// For this you need to include:
+// One button to represent Reset Count in followers_count.html file.
+// Then create one function in followers_count.js file
+// to reset count to 0.
+// Also use alert popup box method to show alert message
+// which will say that the Followers count has been reset.
